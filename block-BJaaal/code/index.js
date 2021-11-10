a60461e413685d1fdcb4d3f8f8dcf51390b2b007
atul1104 <waghamareatul2@gmail.com>
2021-11-10 11:10:43 +0530

    string template literal assignment

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -7,12 +7,16 @@
 
 let firstName = "Arya";
 let lastName = "Stark";
+ alert("I am" + " " + firstName +" " + lastName + " " + "of winterfall" + ".");
+
+const userName = "Atul";
+let bio = "Learning to code";
 
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
-
+alert ("I am" + " " + userName + " " + "and my bio is" + " " + bio + ".");
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
@@ -20,6 +24,7 @@ let lastName = "Stark";
 
 let amount = 200;
 const taxPercent = 10;
+alert("The total amount is" + " " + amount + " " + "The tax amount is" + " " + [(amount * taxPercent) / 100] );
 
 /* Use string template literal:
   - Alert a message saying `The total amount is [amount]
