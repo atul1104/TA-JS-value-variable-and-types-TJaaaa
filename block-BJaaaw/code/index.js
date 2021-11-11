5747d66ef377a7d58135ec45dded1210153dcff0
atul1104 <waghamareatul2@gmail.com>
2021-11-12 00:05:56 +0530

    Assignment making decisions

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -6,10 +6,31 @@
   - `if` the number is even print the message " number is even"
   - `if` the number is odd print the message "number is odd"
 */
+let num = Number(prompt("Enter a Number"));
+if( num % 2 === 0){
+    alert("Number is Even");
+}else{
+    alert("Number is Odd");
+}
+   
+
 
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
+let numA = Number(prompt("Enter first numbers"));
+let numB = Number(prompt("Enter second number"));
+if(numA > numB){
+    alert(numA);
+}else{
+    alert(numB);
+  }
 
 // 3. Convert the above code using`?` terniary operator
+let numA = Number(prompt("Enter first numbers"));
+let numB = Number(prompt("Enter second number"));
+numA > numB ?
+ alert (numA)
+ :
+ alert (numB)
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -18,13 +39,64 @@
   - `if` house name is "lannister" then print the message " A lannister always pays his debt"
   - `else` print the message " All men must die"
 */
+let house_name = prompt("Enter house name");
+if (house_name === "stark"){
+  alert ("winter is coming");
+} else if (house_name === "Lannister") {
+  alert("A Lannister always pays his debt");
+} else { alert("All men must die");}
 
 // 5. Convert the above code using`?` terniary operator
+let house_name = prompt("Enter house name");
+house_name === "stark" ?
+ alert ("winter is coming")
+ :
+ alert("A Lannister always pays his debt")
+
 
 // Switch
 
 // 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
 
+let month_name = prompt("Enter a Month");
+switch(true) {
+    case month_name == "1":
+    alert(31);
+    break;
+      case month_name === "2":
+    alert("28");
+    break;
+      case month_name === "3":
+    alert("31");
+    break;
+      case month_name === "4":
+    alert("30");
+    break;
+      case month_name === "5":
+    alert("31");
+    break;
+      case month_name === "6":
+    alert("30");
+    break;
+      case month_name === "7":
+    alert("31");
+    break;
+      case month_name === "8":
+    alert("31");
+    break;
+      case month_name === "9":
+    alert("30");
+    break;
+      case month_name === "10":
+    alert("31");
+    break;
+      case month_name === "11":
+    alert("30");
+    break;
+     case month_name === "12":
+    alert("31");
+    break;
+}
 /* 7.
 - Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
   - `Salary <= 20000` tax is 10 %
@@ -32,6 +104,14 @@
   - `Salary > 50000` tax is 30 %
 
 */
+let salary = Number(prompt("Enter the salary amount"));
+if (salary <= 20000){
+    alert (salary - (salary * (10/100)));
+} else if (salary <= 40000){
+    alert (salary - (salary * (20/100)));
+} else if (salary > 50000){
+    alert (salary - (salary * (30/100)));
+}  
 
 //  if..else vs switch
 
@@ -45,6 +125,37 @@ Implement the condition give below using`if..else` and`switch` statement.
   - `marks > 0` alert`"Grade D"`
 
 */
+let marks = Number(prompt("Enter your marks"));
+if(marks > 100){
+    alert ("Marks can't be greater than 100");
+} else if( marks > 80 && marks < 100){
+    alert ("Grade A");
+} else if ( marks > 50 && marks < 80){
+    alert ("Grade B");
+} else if( marks > 30 && marks < 50){
+    alert ("Grade C");
+} else if (marks > 0){
+    alert("Grade D");
+}
+
+// Switch method
+let marks = Number(prompt("Enter your marks"));
+switch (marks){
+    case marks > 100:
+    alert ("Grade A");
+    break;
+    case  marks > 80 && marks < 100:
+     alert ("Grade A");
+    break; 
+    case marks > 50 && marks < 80:
+     alert ("Grade B");
+     break;
+    case marks > 30 && marks < 50:
+      alert ("Grade C");
+      break;
+     case marks > 0:
+      alert("Grade D");
+}
 
 /* 9. Weather app
 
