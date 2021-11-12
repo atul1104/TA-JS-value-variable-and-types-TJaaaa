774fdaeb759bdf8b279094362b40799204389b8e
atul1104 <waghamareatul2@gmail.com>
2021-11-12 12:33:48 +0530

    Assignment level2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,11 +5,19 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user = prompt("Enter your name");
+alert(user);
+let userName = (user);
+console.log(user + userName);
+let age = 22; let isLearning = true;
+let user = ("John");
+console.log(user + userName);
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
-
+let numA = Number(prompt("Enter the first Number"));
+let numB = Number(prompt("Enter the second Number"));
+let sum = (numA + numB)
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
